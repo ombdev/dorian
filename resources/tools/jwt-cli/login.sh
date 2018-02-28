b263b02fb50f2d805fadf4447295796a65d7eb18
ombdev <omontes.dev@gmail.com>
2018-02-28 17:59:23 -0600

    Update login.sh

--- resources/tools/jwt-cli/login.sh
+++ resources/tools/jwt-cli/login.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-
-function login {
-    AUTH_TARGET="http://$3/token-auth"
-    curl -H "Content-Type: application/json" -X POST -d \
-        "{\"Username\":$1,\"Password\":$2}" $AUTH_TARGET
-}
-
-
-login $1 $2 $3
+curl -X POST -H "Content-Type: application/json" -d "{\"Username\": \"$1\", \"Password\": \"$2\"}" "http://$3/token-auth"
